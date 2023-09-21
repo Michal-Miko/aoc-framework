dd7c3c04b5fe9b3b9b4a34122fc681715b8ccc91
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2023-09-21 10:07:29 +0200

    Boxed tasks (#1)
    * Boxed Tasks
    * Added a function to check a Vec of AocTasks

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,52 @@
 pub mod error;
 mod task;
 
+use error::AocError;
 pub use task::{AocIO, AocInput, AocSolution, AocTask};
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub type BoxedAocTask = Box<dyn AocTask>;
+
+pub fn check_solved_tasks(
+    tasks: Vec<BoxedAocTask>,
+    phases_per_task: usize,
+) -> Result<bool, AocError> {
+    for (i, task) in tasks.iter().enumerate() {
+        let example_test_result = task.run_example_test()?;
+        if !example_test_result.passed {
+            println!("✘ {} failed the example test.", task.name());
+            println!(
+                "Expected output:\n{:#?}",
+                example_test_result.expected_output
+            );
+            println!("Solution output:\n{:#?}", example_test_result.output);
+            return Ok(false);
+        } else {
+            println!("✔ {} passed the example test!", task.name());
+        }
+
+        let solution_output = task.solve()?;
+        println!("· Solution attempt:\n{}", solution_output.join("\n"));
+
+        for phase in 1..=phases_per_task {
+            let solved = task.ask_if_solved(phase)?;
+            if !solved {
+                println!(
+                    "✘ Phase {phase}/{phases_per_task} of {} failed.",
+                    task.name()
+                );
+                return Ok(false);
+            } else {
+                println!(
+                    "✔ Phase {phase}/{phases_per_task} of {} solved!",
+                    task.name()
+                );
+            }
+        }
+
+        println!("✔ Task {} - {}/{} done!", task.name(), i + 1, tasks.len());
+        println!("=================================================");
+    }
+
+    println!("🚀🚀🚀✔️ All tasks have been completed! ✔️🚀🚀🚀");
+    Ok(true)
 }
